5f1cbccc977e0ba6a86cc708d0c1837a6a0d3b25
Robson Bomfim <robsombomfim@gmail.com>
2022-01-25 10:52:01 -0300

    First Model-Controller-View - Department

---
+++ WebSales/Controllers/DepartmentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebSales.Models;
+
+namespace WebSales.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> departments = new List<Department>()
+            {
+                new Department{ Id = 1, Name = "Eletronics"},
+                new Department { Id = 2, Name = "Fashion" }
+            };
+
+            return View(departments);
+        }
+    }
+}
